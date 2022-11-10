7759232bfd2d9b1c58ca108470843c3d2f23b984
leerivera <leerivera80@gmail.com>
2022-11-10 15:24:56 -0500

    send

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const  express = require('express')
 const path = require('path')
 const app = express()
 const port = 3000
-
+// setup static and middleware
 app.use(express.static('./public'))
 
 app.get('/', (req, res)=> {
