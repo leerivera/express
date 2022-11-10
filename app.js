f0b5efe21c6af024762529def7a115bcfae05fb5
leerivera <leerivera80@gmail.com>
2022-11-10 15:34:06 -0500

    basic api

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-const  express = require('express')
-const path = require('path')
+const express = require('express')
 const app = express()
 const port = 3000
-// setup static and middleware
-app.use(express.static('./public'))
 
-app.get('/', (req, res)=> {
-    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
-})
+app.get('/', (req, res) => res.json([{ name: 'John'}, {name: 'susan'}]))
 
-app.get('/about', (req, res)=> {
-    res.status(200).send("about page")
-})
-
-app.all('*', (req, res)=>{
-    res.status(404).send('<h1> resourse not found</h1>')
-})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
