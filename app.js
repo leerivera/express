a6e1b8f94aebb2291bc3706a7bc84f4fb164b200
leerivera <leerivera80@gmail.com>
2022-11-10 15:11:06 -0500

    public

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const  express = require('express')
+const path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('./public'))
+
+app.get('/', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './navbar-app/index.html'))
+})
+
+app.get('/about', (req, res)=> {
+    res.status(200).send("about page")
+})
+
+app.all('*', (req, res)=>{
+    res.status(404).send('<h1> resourse not found</h1>')
+})
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
