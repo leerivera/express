72896370825b41da73d7c633efe2db3fd42373b1
leerivera <leerivera80@gmail.com>
2022-11-11 14:36:33 -0500

    search

--- app.js
+++ app.js
@@ -32,7 +32,14 @@ app.get('api/products/:productID/reviews/:reviewID', (req, res) => {
 
 app.get('api/v1/query', (req, res) => {
     console.log(req.query)
+    const { search, limit } = req.query
     let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter((product) => {
+            return product.name.startsWith(search)
+        })
+    }
     res.send('hello world')
 })
 
