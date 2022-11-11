0d547a7b34e9c4d3ab7670d3f16d1b30626dbb54
leerivera <leerivera80@gmail.com>
2022-11-11 14:17:06 -0500

    singleproduct

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
+const { application } = require('express')
 const express = require('express')
 const app = express()
+const {products} = require('./data')
 const port = 3000
 
-app.get('/', (req, res) => res.json([{ name: 'John'}, {name: 'susan'}]))
+app.get('/', (req, res) => {
+    res.json('<h1> Home Page </h1><a href="/api/products">products</a>')
+})
+
+app.get('/api/products', (req, res) => {
+    const newProducts = products.map((products)=>{
+        const {id, name, image} = products
+        return {id,name, image}
+    })
+    res.json(newProducts)
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
