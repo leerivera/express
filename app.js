adac98a3ef7eaff173558e594f2aa6d40f71866a
leerivera <leerivera80@gmail.com>
2022-11-11 14:43:33 -0500

    more

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get('/api/products', (req, res) => {
     res.json(newProducts)
 })
 
-app.get('api/products/:productID', (req, res)=> {
+app.get('/api/products/:productID', (req, res)=> {
     const { productID } = req.params;
 
     const singleProduct = products.find((product) => product.id === Number(productID))
@@ -24,13 +24,13 @@ app.get('api/products/:productID', (req, res)=> {
     res.json(singleProduct)
 })
 
-app.get('api/products/:productID/reviews/:reviewID', (req, res) => {
+app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
     console.log(req.params)
     
     res.send('hello world')
 })
 
-app.get('api/v1/query', (req, res) => {
+app.get('/api/v1/query', (req, res) => {
     console.log(req.query)
     const { search, limit } = req.query
     let sortedProducts = [...products]
@@ -44,6 +44,7 @@ app.get('api/v1/query', (req, res) => {
     if(limit){
         sortedProducts = sortedProducts.slice(0,Number(limit))
     }
+    res.status(200).json(sortedProducts)
     res.send('hello world')
 })
 
