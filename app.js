75127389ccc8bbb51db31f1a5411b3a899b4ae63
leerivera <leerivera80@gmail.com>
2022-11-11 16:43:02 -0500

    authorize

--- app.js
+++ app.js
@@ -1,23 +1,30 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const logger =require('./logger')
+const authorize = require('./authorize')
+
+app.use([authorize, logger])
+//if no route is chosen the middleware will be applied everywhere
 
 //req => middleware => res
 
-const logger = (req, res, next) => {
-    
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time)
-    res.send('Testing')
-}
 
-app.get('/', logger,(req, res) => {
+app.get('/',(req, res) => {
  
   res.send("Home") 
 })
 
-app.get('/about', (req, res) => res.send('About page'))
+app.get('/about',  (req, res) => res.send('About page'))
+
+app.get('/api/products', (req, res) => {
+ 
+    res.send("products") 
+  })
+
+  app.get('/api/items', (req, res) => {
+ 
+    res.send("Items") 
+  })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
---
+++ authorize.js
@@ -0,0 +1,7 @@
+
+const  authorize = (req, res, next) => {
+    console.log('authorize')
+  next()
+}
+
+module.exports = authorize
