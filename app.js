19f1f2ef387bf8a95f6eb52475d46a3258207a27
leerivera <leerivera80@gmail.com>
2022-11-13 19:55:26 -0500

    delete

--- app.js
+++ app.js
@@ -62,5 +62,14 @@ app.put('/api/people/:id', (req, res) => {
     res.status(200).json({ success: true, data: newPeople})
 })
 
+app.delete('api/people/:id', (req, res) => {
+    const person = people.find((person) => person.id === Number(req.params.id))
+    if(!person){
+        return res.status(404).json({ success: false, msg: `no person with id ${req.params}`})
+    }
+    const newPeople = people.filter((person) => person.id !== Number(req.params.id))
+    return res.status(200).json( { sucess: true, data: newPeople }) 
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
